fd1f154e753b4ba7814b318a114baa7416b9aabb
Niklas <me@sodapop.se>
2019-09-19 11:00:56 +0200

    Update benchmarks

--- files/sleeper.sh
+++ files/sleeper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sleep 5
+echo "sleep?"
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const puppeteer = require("puppeteer");
+const process = require("process");
 const path = require("path");
+const fs = require("fs");
 const uuidv4 = require("uuid/v4");
 
 const assignment_title = "Benchmark";
 const assignment_id = uuidv4();
+const test_count = 3;
+const browser_count = 3;
+
+const NS_PER_SEC = 1e9;
+
+const headless = true;
 
 function delay(timeout) {
   return new Promise(resolve => {
@@ -14,8 +22,8 @@ function delay(timeout) {
 const lti_launcher_url = `file:${path.join(__dirname, "../server/lti_launcher/index.html")}`;
 
 (async () => {
-  let browser = await puppeteer.launch({ headless: false });
-  let [page] = await browser.pages();
+  let browser = await puppeteer.launch({ headless });
+  const page = (await browser.pages())[0];
 
   page.on("dialog", async dialog => {
     await dialog.accept();
@@ -38,61 +46,102 @@ const lti_launcher_url = `file:${path.join(__dirname, "../server/lti_launcher/in
   );
 
   await page.click("#launch");
-  await delay(500);
-  const autocheck_teacher_page = (await browser.pages())[1];
+
+  const pageTarget = page.target();
+  const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
+
+  const autocheck_teacher_page = await newTarget.page();
 
   // Configure course
 
-  await autocheck_teacher_page.evaluate(() => {
-    document.querySelector("#dsl").value = `
-      @env "elixir",
-        version: "1.7"
-
-      step "Run sleeper" do
-        run "chmod +x sleeper.sh"
-        run "./sleeper.sh"
-      end
-    `;
-  });
+  // await delay(1500);
+  await autocheck_teacher_page.waitForFunction(() => window.monaco && window.monaco.editor.getModels().length > 0);
+  await autocheck_teacher_page.evaluate(configuration => {
+    window.monaco.editor.getModels()[0].setValue(configuration);
+  }, fs.readFileSync("./files/Autocheckfile", "utf8"));
+  // await delay(1500);
 
   // Save configuration
   await autocheck_teacher_page.click("#configuration-form button[type='submit']");
   await autocheck_teacher_page.close();
 
-  // Now auth as student
-  await page.evaluate(() => {
-    document.querySelector("select[name='roles']").selectedIndex = 1;
-  });
-
-  await page.click("#launch");
-  await delay(500);
-
-  const autocheck_student_page = (await browser.pages())[1];
-  await autocheck_student_page.close();
-
-  let start = new Date();
-
-  const pages = [];
-
-  for (let i = 0; i < 5; i++) {
-    pages.push(
-      browser.newPage().then(async page => {
-        await page.goto("https://localhost:4001/submission/submit/" + assignment_id);
-        const fileInput = await page.$("input[type=file]");
-        await fileInput.uploadFile("./files/sleeper.sh");
-
-        await page.click("button[type='submit']");
-        await page.waitFor("i.green.check.icon");
-        await page.close();
-      })
-    );
-  }
-
-  await Promise.all(pages);
-
-  let end = new Date();
-
-  console.log("This took", end - start, "ms");
-
   await browser.close();
+
+  console.log("Assignment configured");
+
+  let browsers = [];
+  for (let i = 1; i <= test_count; i++) {
+    for (let j = 1; j <= browser_count * i; j++) {
+      browsers.push(
+        (async () => {
+          const browser = await puppeteer.launch({ headless });
+          const page = (await browser.pages())[0];
+
+          page.on("dialog", async dialog => {
+            await dialog.accept();
+          });
+
+          await page.goto(lti_launcher_url);
+          await page.evaluate(() => {
+            document.querySelector("select[name='roles']").selectedIndex = 1;
+          });
+          await page.click("#newtab");
+          await page.click("#launch");
+
+          const pageTarget = page.target();
+          const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
+
+          const autocheck_student_page = await newTarget.page();
+          await autocheck_student_page.goto("https://localhost:4001/submission/submit/" + assignment_id);
+          const fileInput = await autocheck_student_page.$("input[type=file]");
+          await fileInput.uploadFile("./files/sleeper.sh");
+
+          return [browser, autocheck_student_page];
+        })()
+      );
+    }
+
+    const browsers_with_pages = await Promise.all(browsers);
+
+    console.log("Spawned", browsers_with_pages.length, "browsers");
+
+    const measurements = browsers_with_pages.map(async ([browser, page]) => {
+      await Promise.all([page.click("button[type='submit']"), page.waitForNavigation()]);
+      const start = process.hrtime.bigint();
+      await page.waitFor("i.green.check.icon", { timeout: 3600 * 1000 });
+
+      const end = process.hrtime.bigint();
+
+      return Number(end - start);
+    });
+
+    let all_measurements = Promise.all(measurements).then(measurements => [browser_count * i, measurements]);
+    let timeout = delay(100 * 1000).then(() => [browser_count * i, "timeout"]);
+
+    let [num_browsers, result] = await Promise.race([all_measurements, timeout]);
+
+    if (result != "timeout") {
+      const median_ns = median(result);
+      const mean_ns = mean(result);
+
+      console.log("#browsers=", num_browsers, "median=", median_ns / 1000000, "ms", "mean=", mean_ns / 1000000, "ms");
+    } else {
+      console.log("#browsers=", num_browsers, "timeout");
+    }
+
+    console.log("Closing", browsers_with_pages.length, "browsers");
+    await Promise.all(browsers_with_pages.map(([browser, _]) => browser.close()));
+
+    browsers = [];
+  }
 })();
+
+function median(array) {
+  array.sort((a, b) => a - b);
+  var mid = array.length / 2;
+  return mid % 1 ? array[mid - 0.5] : (array[mid - 1] + array[mid]) / 2;
+}
+
+function mean(array) {
+  return array.reduce((sum, value) => sum + value, 0) / array.length;
+}
